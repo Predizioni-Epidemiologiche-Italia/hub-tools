82d5efcea24ec1912a32ca0b4de048a30643b0bd
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2023-11-21 11:45:52 +0100

    Create authenticate_config.py

---
+++ .github/scripts/request_authentication /authenticate_config.py
@@ -0,0 +1,7 @@
+#############################################################
+### Local Config file for the authenticate python module  ###
+#############################################################
+
+# matching file configuration
+default_saving_path = 'previsioni'
+default_mapping_file = 'authenticate-mapping.json'
