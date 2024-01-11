6eb9d792ac5cb8836c17dc294fcab34430ee73a8
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2024-01-11 16:30:30 +0100

    Update validate.py

--- .github/scripts/forecast_validation/validate.py
+++ .github/scripts/forecast_validation/validate.py
@@ -4,6 +4,14 @@ import re
 import json
 import sys
 
+from datetime import datetime, timedelta
+from isoweek import Week
+
+# list of days in a week
+weekdaysList = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+                'Friday', 'Saturday', 'Sunday']
+
+
 import validate_forecasts as v
 
 
@@ -15,6 +23,45 @@ def outputResults (result = True, result_msg = "" ):
         print (f"Writing results to output. Validate: {out_res}, msg: {result_msg}")
         outenv.write (f"validate={out_res}\n")
         outenv.write (f"message={result_msg}")
+
+# This function returns the last weekday of input day by
+# accepting the input day as argument
+def getLastByDay  (inputDay):
+    # Get today's date
+    today = datetime.today()    
+    # getting the last weetodaykday
+    daysAgo = (today.weekday() - weekdaysList.index(inputDay)) % 7
+
+    # Subtract the above number of days from the current date(start date)
+    # to get the last week's date of the given day
+    targetDate = today - timedelta(days=daysAgo)
+
+    return targetDate
+
+def is_in_submit_window (submitting_elem):
+
+    print (f'verifying submission window for {submitting_elem}')
+    
+    # Calculate last Friday date
+    last_friday = getLastByDay('Friday')
+
+    # Calculate the current week's Tuesday
+    this_tuesday = last_friday + timedelta(days=4)
+
+    # Check if the date is between last Friday and this Thursday
+    if not (last_friday <= datetime.today() <= this_tuesday):
+        raise RuntimeError ("Submission time must be within accepted submission window for round.")
+
+    
+    reference_week = Week.withdate(last_friday) - 1
+    # get forecasting year and week from the file name
+    year_week = submitting_elem.split('/')[-1].split('.')[0].split('_')
+    uploading_week = Week.fromstring(year_week[0] + "W" + year_week[1])
+
+    if uploading_week != reference_week:
+        raise RuntimeError ("Forecasting week must be within accepted submission window for round.")
+
+
 
 
 def run ():
@@ -28,6 +75,10 @@ def run ():
         print ("Validating {}".format(elem))
 
         try:
+            # verify if still in the submission window
+            is_in_submit_window (submitting_elem=elem)
+
+            # then verify that forma is valid
             v.validate_csv_files("influcast_flu_forecast", elem)
             outputResults()
 
