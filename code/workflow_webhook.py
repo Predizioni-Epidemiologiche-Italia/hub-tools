d1965169aa97f6d359b2d0d5f5edb2ea2be63172
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2026-08-13 15:58:59 +0200

    Update workflow_webhook.py

--- code/workflow_webhook.py
+++ code/workflow_webhook.py
@@ -170,8 +170,8 @@ def run ():
         jpayload["season"] = jdata["season"]
         jpayload["targets"] = jdata["targets"]
     elif data_type == 'influmeter':
-        # jpayload["influmeter"] = jdata
-        jpayload = jdata
+        jpayload["influmeter"] = jdata["changes"]
+        # jpayload = jdata
     else:
         print (f'Unsupported submission datatype: {data_type}')
         exit(1)
